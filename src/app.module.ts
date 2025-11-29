5cdf2e553f2d85b670f490620be1e6ef5729857f
Rayed Ahmed <arayed352@gmail.com>
2025-11-29 18:38:35 +0600

    fix initialize project nameing issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { ServiceProviderModule } from './service-provider/service-provider.module';
 import { AdminModule } from './admin/admin.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [UserModule, ServiceProviderModule, AdminModule],
+  imports: [ServiceProviderModule, AdminModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
